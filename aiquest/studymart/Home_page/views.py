7c4edb875285005d042c131db6d56f0003855df4
Md Nuruzzaman <38957494+nuruzzamanhimel00@users.noreply.github.com>
2026-01-31 23:40:32 +0600

    filter and tags

--- aiquest/studymart/Home_page/views.py
+++ aiquest/studymart/Home_page/views.py
@@ -3,4 +3,16 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'Home_page/index.html')
+    offering = {
+       
+        'slider' : {
+             'slider_title': 'Welcome to StudyMart',
+            'slider_subtitle': 'Your Gateway to Quality Education',
+            'slider_image': 'https://images.unsplash.com/photo-1522202176988-66273c2fd55f?w=600',
+            'slider_button_text': 'Explore Courses',
+            'slider_button_text_one' : 'Watch Demo',
+            'slider_cars' : ['car 1', 'car 2', 'car 3'],
+        }
+    }
+    # print(offering)
+    return render(request, 'Home_page/index.html', offering)
